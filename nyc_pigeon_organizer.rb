81e75f46f7216e21d8917d9ac88ca9d2cde7e517
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 23:10:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   
   
-  data.reduce({}) do |memo, (outerkey, innerkey_val))|
+  data.reduce({}) do |memo, (outerkey, innerkey_val)|
     
     pp innerkey_val
     
