a50e51ef0c9583f835ea3e62c6f8c286e947b8d1
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 23:16:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   
   
-  data.reduce({}) do |memo, (outerkey, innerkey)|
+  data.reduce({}) do |memo, outerkey, (innerkey, innerval)|
     
     pp innerkey
     pp "IGNORE THIS LINE"
