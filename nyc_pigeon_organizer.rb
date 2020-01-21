43f85b7cb768e84a598280b80f4fb7ef38b0d045
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 23:39:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,14 +4,14 @@ def nyc_pigeon_organizer(data)
   names = []
   
   open_hash = data.reduce({}) do |memo, outerkey|
-    # key_values << outerkey[0]
-    # innerhash << outerkey[1]
-    names << outerkey[0][outerkey[1]]
+    key_values << outerkey[0]
+    innerhash << outerkey[1]
+    # names << outerkey[0][outerkey[1]]
   end 
   
   # innerhash.reduce() do |memo, innerkey
   
 
-  # puts key_values
-  pp names
+  puts key_values
+  pp innerhash
 end
