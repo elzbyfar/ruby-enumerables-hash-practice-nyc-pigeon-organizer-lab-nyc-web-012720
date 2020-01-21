160a86647d0875fa2afb641715fcb64e16fbf431
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 23:27:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   collect_keys = data.reduce({}) do |memo, outerkey|
     key_values << outerkey[0]
     names << memo[outerkey]
+    pp memo[outerkey]
   end 
 
   # puts key_values
