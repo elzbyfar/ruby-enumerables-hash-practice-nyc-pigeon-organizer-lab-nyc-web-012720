cc5f30360034505604da7d4bf109ef05bf42eaad
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 23:07:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
 
-  data.reduce({}) do |memo, (outerkey, outerval|
+  data.reduce({}) do |memo, (outerkey, outerval, (innerkey, innerval))|
     
     pp outerval
     
