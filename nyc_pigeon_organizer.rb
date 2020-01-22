54fa82915c1e455a159902d84cd761bf708400fe
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-22 03:50:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,34 +15,11 @@ def nyc_pigeon_organizer(data)
           new_hash[name][att] = att_val_arr
         else 
           new_hash[name] = small_hash
-        
         end 
       end 
     end 
   end 
   
 new_hash  
-  
-  
-  
-  # data.reduce({}) do |memo, (col_gen_liv, purp_male_sub)|
-  #   purp_male_sub.each_pair do |purpkey, names_arr|
-      
-  #     tail_hash = Hash.new 
-  #     tail_hash[col_gen_liv] = purpkey
-      
-  #     names_arr.map do |name| 
-  #       if memo[name]
-  #         memo[name][col_gen_liv] = purpkey
-  #       else 
-  #         memo[name] = tail_hash
-  #       end 
-        
-  #     end 
-      
-  #   end 
-  #   #   pp memo
-  #   # memo
-  # end
     
 end
