85eef03e3e4db160acd2c9a240b9fd91e53a7656
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-22 00:04:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   
   data.reduce({}) do |memo, (outermost_key, innerhash)|
     innerhash.each_pair do |innerkey, names_arr|
-      # memo[outermost_key] = innerkey
+        
       pp innerkey
     end 
     
