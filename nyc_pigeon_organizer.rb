106f070f659e515ecadcb36fd087f85f96fd1d4c
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-22 03:56:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,15 +6,16 @@ def nyc_pigeon_organizer(data)
       name_arr.each do |name| 
         att_val_arr = []
         att_val_arr << att_val.to_s
-        small_hash[att] = att_val_arr
       
         if new_hash[name] && new_hash[name][att]
           new_hash[name][att] << att_val.to_s 
+          
         elsif new_hash[name] && !new_hash[name][att]
           new_hash[name][att] = att_val_arr
+          
         else 
           small_hash = {}
-
+          small_hash[att] = att_val_arr
           new_hash[name] = small_hash
         end 
       end 
